b837c031bb6a06fbc6fa5e5617dd2871a86a4a59
yshls <shluxnsal01@gmail.com>
2025-07-01 00:41:52 +0900

    Feat: App.jsx에 Route 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import New from './pages/New';
+import Diary from './pages/Diary';
 
 // 1: "/": 모든 일기를 조회하는 Home 페이지
 // 2: "/new": 새로운 일기를 작성하는 new페이지
 // 3: "/diary": 일기를 상세히 조회하는 Diary 페이지
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/" element={<New />}></Route>
+      <Route path="/" element={<Diary />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
