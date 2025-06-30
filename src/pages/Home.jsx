5e2bcbf7f2c128940067c9ab49d55eab1393203b
yshls <shluxnsal01@gmail.com>
2025-07-01 01:27:31 +0900

    feat: useSearchParams

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+/** useSearchParams: QueryString의 값을 불러옴 */
+import { useSearchParams } from 'react-router-dom';
 
 const Home = () => {
+  const [params, setParams] = useSearchParams();
+  console.log(params.get('value'));
   return <div>Home</div>;
 };
 
